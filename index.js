f173513044123812df47f9aad3911b7b7ad1195d
Angel Tyler <102939438+angeltyler01@users.noreply.github.com>
2022-07-20 11:03:32 -0700

    Addeed js code to make character move.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const pc = sumoCharacter(100,110)
+const pc = document.getElementById("sumoCharacter");
+
+let modifier = 5;
+window.addEventListener('keydown', (event) => {
+   const { style } = sumoCharacter;
+   switch (event.key) {
+       case 'ArrowUp': style.top = `${parseInt(style.top) - modifier}px`; break;
+       case `ArrowDown` : style.top = `${parseInt(style.top) + modifier}px`; break;
+       case 'ArrowLeft' : style.left = `${parseInt(style.left) - modifier}px`; break;
+       case 'ArrowRight' : style.left = `${parseInt(style.left) + modifier}px`; break; 
+   }
+
+});
+
 
